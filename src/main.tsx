0e8ff570312ef4b49d2b5dc7409d7887818ae3d3
Aaron Ryu <fbcndah@gmail.com>
2025-04-09 14:58:23 +0900

    routes setting t0 t1 s1 s2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import TabletZeroLayout from "./layouts/TabletZeroLayout.tsx";
+import TabletOneLayout from "./layouts/TabletOneLayout.tsx";
+import ScreenOneLayout from "./layouts/ScreenOneLayout.tsx";
+import ScreenTwoLayout from "./layouts/ScreenTwoLayout.tsx";
+import Main from "./pages/Main.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/t0" element={<TabletZeroLayout />}>
+          <Route path="main" element={<Main />} />
+          <Route path="*" element={<Navigate to="/t0" replace />} />
+        </Route>
+
+        <Route path="/t1" element={<TabletOneLayout />}>
+          <Route index element={<Main />} />
+          <Route path="*" element={<Navigate to="/t1" replace />} />
+        </Route>
+
+        <Route path="/s1" element={<ScreenOneLayout />}>
+          <Route index element={<Main />} />
+          <Route path="*" element={<Navigate to="/s1" replace />} />
+        </Route>
+
+        <Route path="/s2" element={<ScreenTwoLayout />}>
+          <Route index element={<Main />} />
+          <Route path="*" element={<Navigate to="/s2" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
